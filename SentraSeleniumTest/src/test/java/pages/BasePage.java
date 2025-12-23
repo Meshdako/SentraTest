0503c236a88b0be08a7acd169f2f1d9239f0ac7b
Martín Sobarzo <meshdako@proton.me>
2025-12-23 19:52:54 -0300

    Buenas prácticas
    He quitado el @AfterClass del TestRunner y lo dejé dentro del nuevo archivo Hooks y dentro de este nuevo archivo inicializo el driver para luego dejar un @After el cual cierra la instancia del driver. Aún sigo luchando, porque cierra muy rápido la instancia cuando finaliza una prueba y no deja ni siquiera visualizar si se realizó el cambio, además que tuve que dejarle un wait de 60 segundos, porque el de 15 cerraba muy antes la instancia.
    Corregí el archivo BasePage dejando la instancia wait después de la creación del driver, además, que dejé el constructor del driver que se inicializara una sola vez y con eso no se creaba constantemente en cada pages. Dentro de este mismo archivo corregí el apartado para borrar el contenido de un input, dejando una solución que no conocía, pero permite borrar el contenido bien, ya que el anterior, .clear(), no borraba nada y dejaba que todo igual sumándole el nuevo contenido.

--- SentraSeleniumTest/src/test/java/pages/BasePage.java
+++ SentraSeleniumTest/src/test/java/pages/BasePage.java
@@ -1,15 +1,17 @@
 package pages;
- 
+
 // Importaciones necesarias
 import java.time.Duration;
 import java.util.List;
 
+import org.jspecify.annotations.NonNull;
+import org.junit.rules.ExpectedException;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.*;
- 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
- 
+
 public class BasePage {
     /*
      * Declaración de una variable estática 'driver' de tipo WebDriver
@@ -17,83 +19,87 @@ public class BasePage {
      * subclases
      */
     protected static WebDriver driver;
-    /*
-     * Declaración de una variable de instancia 'wait' de tipo WebDriverWait.
-     * Se inicializa inmediatamente con una instancia dew WebDriverWait utilizando
-     * el 'driver' estático
-     * WebDriverWait se usa para poner esperas explícitas en los elementos web
-     */
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
- 
-    /*
-     * Configura el WebDriver para Chrome usando WebDriverManager.
-     * WebDriverManager va a estar descargando y configurando automáticamente el
-     * driver del navegador
-     */
-    static {
-        WebDriverManager.chromedriver().setup();
- 
-        // Inicializa la variable estática 'driver' con una instancia de ChromeDriver
-        driver = new ChromeDriver();
+    
+    // WebDriverWait se usa para poner esperas explícitas en los elementos web
+    WebDriverWait wait;
+
+    // Creamos nuestro constructor sin parámetros, así el driver se crea solo una vez.
+    public BasePage() {
+        if( driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        
+        wait = new WebDriverWait(driver, Duration.ofSeconds(60));
     }
- 
-    /*
-     * Este es el constructor de BasePage que acepta un objeto WebDriver como
-     * argumento.
-     */
-    public BasePage(WebDriver driver) {
-        BasePage.driver = driver;
-    }
- 
+
     // Método estático para navegar a una URL.
     public static void navigateTo(String url) {
         driver.get(url);
     }
- 
-    //Método estático para cerrar la instancia del driver. 
+    
+    // Método estático para cerrar la instancia del driver.
     public static void closeBrowser() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
- 
+
     // Encuentra y devuelve un WebElement en la página utilizando un locator XPath,
     // esperando a que esté presentente en el DOM
     private WebElement Find(String locator) {
-        return wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
     }
- 
+
     public void clickElement(String locator) {
         Find(locator).click();
     }
- 
+
+    public boolean message(String locator) {
+        try {
+            return Find(locator).isDisplayed();
+        } catch (Exception e) {
+            // Si el elemento no existe o no aparece, retorna false
+            return false;
+        }
+    }
+
     /*
-    La función write permite borrar lo que hay dentro del campo de texto y escribe el nuevo texto que se le entregó.
-    */
-    public void write(String locator, String keysToSend){
-        Find(locator).clear();
+     * La función write permite borrar lo que hay dentro del campo de texto y
+     * escribe el nuevo texto que se le entregó.
+     */
+    public void write(String locator, String keysToSend) {
+        Find(locator).click();
+        Find(locator).sendKeys(Keys.CONTROL + "a");
+        Find(locator).sendKeys(Keys.DELETE);
         Find(locator).sendKeys(keysToSend);
     }
-    
+
     /*
-    Funciones que buscan un elemento dentro de una lista que se despliega. Dicha búsqueda es por valor, por índice y la función final entrega el tamaño de la lista en cuestión.
-    */
-    public void selectFromDropdownByValue(String locator, String value){
+     * Funciones que buscan un elemento dentro de una lista que se despliega. Dicha
+     * búsqueda es por valor, por índice y la función final entrega el tamaño de la
+     * lista en cuestión.
+     */
+    public void selectFromDropdownByValue(String locator, String value) {
         Select dropdown = new Select(Find(locator));
- 
+
         dropdown.selectByValue(value);
     }
- 
-    public void selectFromDropdownByIndex(String locator, Integer index){
+
+    public void selectFromDropdownByIndex(String locator, Integer index) {
         Select dropdown = new Select(Find(locator));
- 
+
         dropdown.selectByIndex(index);
     }
- 
-    public int dropdownSize(String locator){
+
+    public int dropdownSize(String locator) {
         Select dropdown = new Select(Find(locator));
- 
+
         List<WebElement> dropdownOptions = dropdown.getOptions();
- 
+
         return dropdownOptions.size();
     }
- 
+
 }
--- SentraSeleniumTest/src/test/java/runner/TestRunner.java
+++ SentraSeleniumTest/src/test/java/runner/TestRunner.java
@@ -1,11 +1,9 @@
 package runner;
  
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
  
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import pages.BasePage;
  
 @SuppressWarnings("deprecation")
 @RunWith(Cucumber.class)
@@ -14,8 +12,5 @@ import pages.BasePage;
                 plugin = { "pretty", "html:target/cucumber-reports" })
  
 public class TestRunner {
-        /* @AfterClass
-        public static void cleanDriver() {
-                BasePage.closeBrowser();
-        } */
+        
 }
