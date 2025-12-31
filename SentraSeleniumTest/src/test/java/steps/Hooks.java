cfe69b5f0f9f48e6d2e22a2f03efd7a9b34daa8e
Martín Sobarzo <meshdako@proton.me>
2025-12-30 21:29:27 -0300

    Archivos de configuración
    Estos archivos simplemente fueron actualizados y algunos son parte del propio trabajo.

--- SentraSeleniumTest/src/test/java/steps/Hooks.java
+++ SentraSeleniumTest/src/test/java/steps/Hooks.java
@@ -1,8 +1,5 @@
 package steps;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -18,7 +15,12 @@ public class Hooks {
 
     @After
     public void tearDown(Scenario scenario) {
-        BasePage.takeANewScreenshot(scenario);
-        BasePage.closeBrowser();
+        try {
+            Thread.sleep(2000);
+            BasePage.takeANewScreenshot(scenario);
+            BasePage.closeBrowser();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
