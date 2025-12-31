b140424f38296688973f5644ff57e6d5ffdef298
Martín Sobarzo <meshdako@proton.me>
2025-12-31 01:53:07 -0300

    TestRunner
    No hay muchos cambios, solo fue actualizado este archivo para probar las automatizaciones de forma unitaria con los tags. Ahora que está todo, el tag es comentado para que no provoque problemas.

--- SentraSeleniumTest/src/test/java/runner/TestRunner.java
+++ SentraSeleniumTest/src/test/java/runner/TestRunner.java
@@ -7,10 +7,12 @@ import io.cucumber.junit.CucumberOptions;
  
 @SuppressWarnings("deprecation")
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", // Directorio de nuestros archivos .feature
+@CucumberOptions(
+                // tags = "@solo",            
+                features = "src/test/resources/features", // Directorio de nuestros archivos .feature
                 glue = "steps", // Paquete donde tenemos nuestras clases definiendo los steps
                 plugin = { "pretty", "html:target/cucumber-reports.html" })
  
 public class TestRunner {
-
+    
 }
