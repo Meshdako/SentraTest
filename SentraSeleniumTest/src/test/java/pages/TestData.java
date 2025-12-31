51a55204fafc769e5249445f8550dcb584b6497e
Martín Sobarzo <meshdako@proton.me>
2025-12-30 21:33:27 -0300

    Manejo de datos
    Para no manejar los datos de forma cruda en los archivos de la carpeta pages, fue creada esta clase que maneja los datos, así se mantiene los archivos .feature limpios de información a la vista y los mismos archivos de la carpetas pages solo realizan el llamado.

---
+++ SentraSeleniumTest/src/test/java/pages/TestData.java
@@ -0,0 +1,35 @@
+package pages;
+
+public class TestData {
+    public static final String VALID_EMAIL = "msobarzo@sentra.cl";
+    public static final String VALID_PASSWORD = "msobarzo02";
+
+    public static final String INEXISTING_EMAIL = "ehuerta@sentra.cl";
+    public static final String INEXISTING_PASSWORD = "ehuerta02";
+
+    public static final String NEW_NAME = "Arturo";
+    public static final String NEW_LASTNAME = "Pratt";
+    public static final String NEW_EMAIL = "apratt@sentra.cl";
+    public static final String NEW_PASSWORD = "apratt02";
+
+    public static final String INVALID_NAME = "NombreFalso";
+    public static final String INVALID_LASTNAME = "ApellidoFalso";
+    public static final String INVALID_EMAIL = "correoinvalido@";
+    public static final String INVALID_PASSWORD = "ContraseñaMala";
+    
+    public static final String NEW_WRONG_NAME = "Pepito";
+    public static final String NEW_WRONG_LASTNAME = "Alfonso";
+    public static final String NEW_WRONG_EMAIL = "correo@falso.cl";
+    public static final String NEW_WRONG_PASSWORD_1 = "Contraseña01";
+    public static final String NEW_WRONG_PASSWORD_2 = "Contraseña02";
+
+    public static final String EDIT_NAME = "Jesús";
+    public static final String EDIT_LASTNAME = "De Nazaret";
+
+    public static final String WRONG_PASSWORD_1 = "ProbandoNuevaContraseña";
+    public static final String WRONG_PASSWORD_2 = "NuevaProbandoContraseña";
+
+    public static final String TITLE_INPUT = "Tarea Nuevita";
+    public static final String DESCRIPTION_INPUT = "Nuevo tarea ingresada";
+    public static final String PRIORITY_INPUT = "3";
+}
