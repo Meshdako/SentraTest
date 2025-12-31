2907220ed880f59c305fc3435b8a6d2a06cd3dc9
Martín Sobarzo <meshdako@proton.me>
2025-12-30 21:31:43 -0300

    Nueva clase FilaDeTareas
    Esta clase fue creada para poder trabajar los datos de la tabla de tareas, representa una fila de tareas y con ellos es mucho más fácil el manejo de estos datos. Dentro de la clase Home se trabajan como una lista de FilaDeTareas. Dentro del archivo Home se puede ver la facilidad con la que se trabaja con esta nueva clase.

---
+++ SentraSeleniumTest/src/test/java/pages/FilaDeTareas.java
@@ -0,0 +1,35 @@
+package pages;
+
+import java.time.LocalDateTime;
+
+public class FilaDeTareas {
+    private String title;
+    private LocalDateTime creationDate;
+    private LocalDateTime dueDate;
+    private int priority;
+
+    public FilaDeTareas(String title, LocalDateTime creationDate, LocalDateTime dueDate, int priority) {
+        this.title = title;
+        this.creationDate = creationDate;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    public void imprimirFila() {
+        System.out.print(title + " / ");
+        System.out.print(creationDate + " / ");
+        System.out.print(dueDate + " / ");
+        System.out.print(priority);
+
+    }
+
+    public String getTitle() { return title; }
+    public LocalDateTime getCreationDate() { return creationDate; }
+    public LocalDateTime getDueDate() { return dueDate; }
+    public int getPriority() { return priority; }
+
+    public void setTitle(String title) { this.title =  title; }
+    public void setCreationDate(LocalDateTime creationDate) { this.creationDate = creationDate; }
+    public void setDueDate(LocalDateTime dueDate) { this.dueDate = dueDate; }
+    public void setPriority(int priority) { this.priority = priority; }
+}
